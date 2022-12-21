c465104d6867fcc32d021dbc33b418f319636671
SerhiiSamara <ndaair@gmail.com>
2022-12-21 21:31:54 +0200

    4

--- src/components/ContactForm/ContactForm.styled.jsx
+++ src/components/ContactForm/ContactForm.styled.jsx
@@ -5,7 +5,7 @@ export const Form = styled.form`
   margin-left: 20px;
   margin-top: 20px;
   padding: 20px;
-  border: solid 2px red;
+  border: solid 2px #000;
 `;
 
 export const Label = styled.label`
